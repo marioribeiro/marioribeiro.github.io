a7e26c72ef9e9d9d7fd9d852c5694008fc955e4b
Mario Ribeiro <mail@marioribeiro.com>
2015-09-13 19:37:23 +0100

    Initial commit

---
+++ _posts/2015-08-17-hello-world.markdown
@@ -0,0 +1,40 @@
+---
+layout: post
+title:  "Hello world"
+date:   2015-08-17 20:15:00
+categories: blog
+---
+At the time of this post, I'm 31 years old and I have a confession to make. I've never had a blog.
+
+I decided to make my a try and so I decided to use Jekyll to build my website and obviously use it's blog aware approach and go ahead.
+
+
+To add new posts, simply add a file in the `_posts` directory that follows the convention `YYYY-MM-DD-name-of-post.ext` and includes the necessary front matter. Take a look at the source for this post to get an idea about how it works.
+
+Jekyll also offers powerful support for code snippets:
+
+To add new posts, simply add a file in the `_posts` directory that follows the convention `YYYY-MM-DD-name-of-post.ext` and includes the necessary front matter. Take a look at the source for this post to get an idea about how it works.
+
+Jekyll also offers powerful support for code snippets:
+
+
+
+{% highlight ruby %}
+def print_hi(name)
+  puts "Hi, #{name}"
+end
+print_hi('Tom')
+#=> prints 'Hi, Tom' to STDOUT.
+{% endhighlight %}
+
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+
+Check out the [Jekyll docs][jekyll] for more info on how to get the most out of Jekyll. File all bugs/feature requests at [Jekyll’s GitHub repo][jekyll-gh]. If you have questions, you can ask them on [Jekyll’s dedicated Help repository][jekyll-help].
+
+[jekyll]:      http://jekyllrb.com
+[jekyll-gh]:   https://github.com/jekyll/jekyll
+[jekyll-help]: https://github.com/jekyll/jekyll-help
---
+++ scripts/scripts.js
@@ -0,0 +1,90 @@
+/**** Burger Menu ****/
+
+$(document).ready(function(){
+  $( ".burger-icon" ).click(function() {
+    $( this ).toggleClass( "active" );
+    $(".main-nav").toggleClass("hidden");
+    $(".container").toggleClass("nav-opened");
+    event.preventDefault();
+  });
+});
+
+/**** Nav - Social color hover ****/
+
+$(document).ready(function(){
+  $('.icon-twitter').hover(function(){
+    $('.main-nav').addClass('twitter');
+  },function(){
+    $('.main-nav').removeClass('twitter');
+  });
+  $('.icon-dribbble').hover(function(){
+    $('.main-nav').addClass('dribbble');
+  },function(){
+    $('.main-nav').removeClass('dribbble');
+  });
+  $('.icon-instagram').hover(function(){
+    $('.main-nav').addClass('instagram');
+  },function(){
+    $('.main-nav').removeClass('instagram');
+  });
+  $('.icon-linkedin').hover(function(){
+    $('.main-nav').addClass('linkedin');
+  },function(){
+    $('.main-nav').removeClass('linkedin');
+  });
+  $('.icon-facebook').hover(function(){
+    $('.main-nav').addClass('facebook');
+  },function(){
+    $('.main-nav').removeClass('facebook');
+  });
+});
+
+/*** Back to Top ****/
+
+$(document).ready(function(){
+  // browser window scroll (in pixels) after which the "back to top" link is shown
+  var offset = 400,
+    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+    offset_opacity = 1200,
+    //duration of the top scrolling animation (in ms)
+    scroll_top_duration = 700,
+    //grab the "back to top" link
+    $back_to_top = $('.cd-top');
+
+  //hide or show the "back to top" link
+  $(window).scroll(function(){
+    ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+    if( $(this).scrollTop() > offset_opacity ) {
+      $back_to_top.addClass('cd-fade-out');
+    }
+  });
+
+  //smooth scroll to top
+  $back_to_top.on('click', function(event){
+    event.preventDefault();
+    $('body,html').animate({
+      scrollTop: 0 ,
+      }, scroll_top_duration
+    );
+  });
+});
+
+/**** Waypoints - Animations ****/
+
+$(document).ready(function() {
+  $('.about-image').waypoint(function() {
+  	$('.about-image').addClass('animated fadeInRight');
+  }, {
+  	offset: '80%'
+  });
+  $('.about-intro').waypoint(function() {
+    $('.about-intro').addClass('animated fadeIn');
+  }, {
+    offset: '80%'
+  });
+  $('.about-colophon').waypoint(function() {
+    $('.about-colophon').addClass('animated fadeIn');
+  }, {
+    offset: '70%'
+  });
+});
